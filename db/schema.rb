245a75bacbed0e5515fb4514c82f3c7baa35f571
KaungKaung <aungphyothant291000@gmail.com>
2026-02-03 12:38:53 +0630

    Changed sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_01_26_094649) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
